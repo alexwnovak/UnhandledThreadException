07e6077408f1f694bc15a515dc1f35877e45c382
Alex Novak <alexwnovak@gmail.com>
2020-07-31 16:28:13 -0400

    +WPF project to be the new driver

---
+++ Driver/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Driver
+{
+   public partial class App : Application
+   {
+   }
+}
---
+++ Driver/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Driver
+{
+   public partial class MainWindow : Window
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+   }
+}
