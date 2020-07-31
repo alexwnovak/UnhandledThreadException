af0b331a88cbf768054a3aaf93f8778ecf2e5a10
Alex Novak <alexwnovak@gmail.com>
2020-07-31 16:38:49 -0400

    Hooking up app domain unhandled exception handler

--- Driver/App.xaml.cs
+++ Driver/App.xaml.cs
@@ -1,8 +1,20 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Driver
 {
    public partial class App : Application
    {
+      protected override void OnStartup( StartupEventArgs e )
+      {
+         base.OnStartup( e );
+
+         AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+      }
+
+      private void CurrentDomain_UnhandledException( object sender, UnhandledExceptionEventArgs e )
+      {
+         MessageBox.Show( $"Unhandled exception caught:{Environment.NewLine}{e.ExceptionObject}" );
+      }
    }
 }
